d0584885550d065702044018baf3be59aa7a46f3
Gilles Sadowski <gilles@harfang.homelinux.org>
2019-11-29 03:03:28 +0100

    Unused "import".

--- commons-geometry-hull/src/main/java/org/apache/commons/geometry/euclidean/twod/hull/MonotoneChain.java
+++ commons-geometry-hull/src/main/java/org/apache/commons/geometry/euclidean/twod/hull/MonotoneChain.java
@@ -19,7 +19,6 @@ package org.apache.commons.geometry.euclidean.twod.hull;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 
 import org.apache.commons.geometry.core.precision.DoublePrecisionContext;
