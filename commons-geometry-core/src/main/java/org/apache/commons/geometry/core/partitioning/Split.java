52c517362366e5bafb716fac4c922e587cd62483
Alex Herbert <aherbert@apache.org>
2025-09-23 16:32:22 +0100

    pmd fix: use larger StringBuilder

--- commons-geometry-core/src/main/java/org/apache/commons/geometry/core/partitioning/Split.java
+++ commons-geometry-core/src/main/java/org/apache/commons/geometry/core/partitioning/Split.java
@@ -82,7 +82,7 @@ public final class Split<T> {
     /** {@inheritDoc} */
     @Override
     public String toString() {
-        final StringBuilder sb = new StringBuilder();
+        final StringBuilder sb = new StringBuilder(128);
         sb.append(this.getClass().getSimpleName())
             .append("[location= ")
             .append(getLocation())
--- commons-geometry-core/src/main/java/org/apache/commons/geometry/core/partitioning/bsp/AbstractRegionBSPTree.java
+++ commons-geometry-core/src/main/java/org/apache/commons/geometry/core/partitioning/bsp/AbstractRegionBSPTree.java
@@ -773,7 +773,7 @@ public abstract class AbstractRegionBSPTree<
         /** {@inheritDoc} */
         @Override
         public String toString() {
-            final StringBuilder sb = new StringBuilder();
+            final StringBuilder sb = new StringBuilder(128);
             sb.append(this.getClass().getSimpleName())
                 .append("[cut= ")
                 .append(getCut())
