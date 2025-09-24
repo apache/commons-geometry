e910530bf6811d08995ae525a1c5913ddbef5b35
Alex Herbert <aherbert@apache.org>
2025-09-24 13:10:49 +0100

    pmd fix: use larger StringBuilder

--- commons-geometry-euclidean/src/main/java/org/apache/commons/geometry/euclidean/threed/line/ReverseRay3D.java
+++ commons-geometry-euclidean/src/main/java/org/apache/commons/geometry/euclidean/threed/line/ReverseRay3D.java
@@ -152,7 +152,7 @@ public final class ReverseRay3D extends LineConvexSubset3D {
     /** {@inheritDoc} */
     @Override
     public String toString() {
-        final StringBuilder sb = new StringBuilder();
+        final StringBuilder sb = new StringBuilder(128);
         sb.append(getClass().getSimpleName())
             .append("[direction= ")
             .append(getLine().getDirection())
